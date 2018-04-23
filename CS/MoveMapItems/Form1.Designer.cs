c9a4a57a98116f00032ed630b90ca26a5896c289
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MoveMapItems/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace MoveMapItems {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            DevExpress.XtraMap.VectorItemsLayer vectorItemsLayer1 = new DevExpress.XtraMap.VectorItemsLayer();
+            DevExpress.XtraMap.VectorItemsLayer vectorItemsLayer2 = new DevExpress.XtraMap.VectorItemsLayer();
+            DevExpress.XtraMap.MapItemStorage mapItemStorage1 = new DevExpress.XtraMap.MapItemStorage();
+            DevExpress.XtraMap.MapPushpin mapPushpin1 = new DevExpress.XtraMap.MapPushpin();
+            this.mapControl1 = new DevExpress.XtraMap.MapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mapControl1
+            // 
+            this.mapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            vectorItemsLayer1.Name = "ShapefileLayer";
+            mapPushpin1.Text = "Drag me";
+            mapItemStorage1.Items.Add(mapPushpin1);
+            vectorItemsLayer2.Data = mapItemStorage1;
+            this.mapControl1.Layers.Add(vectorItemsLayer1);
+            this.mapControl1.Layers.Add(vectorItemsLayer2);
+            this.mapControl1.Location = new System.Drawing.Point(0, 0);
+            this.mapControl1.Name = "mapControl1";
+            this.mapControl1.Size = new System.Drawing.Size(761, 464);
+            this.mapControl1.TabIndex = 0;
+            this.mapControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mapControl1_MouseDown);
+            this.mapControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mapControl1_MouseMove);
+            this.mapControl1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mapControl1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 464);
+            this.Controls.Add(this.mapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mapControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraMap.MapControl mapControl1;
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to move a Map Pushpin element at runtime
+
+
+This example illustrates how to make a <strong>Map Pushpin</strong> element movable. To accomplish this task, it is required to convert screen coordinates to map coordinates using the <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapMapControl_ScreenPointToCoordPointtopic"><strong>ScreenPointToCoordPoint</strong></a> method.
+
+<br/>
+
+
